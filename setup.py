1a752fa9b9bce2bada5fba6823c0a842a20cf90a
INUGYN <116585597+INUGYN@users.noreply.github.com>
2023-09-07 21:35:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import os
 import pyshortcuts
+from pyshortcuts import make_shortcut
 from cx_Freeze import setup, Executable
 from pathlib import Path
 
 repertoire = os.path.dirname(os.path.abspath(__file__))
 repertoire = os.path.normpath(repertoire)
 
+# Chemin vers le dossier de destination dans C:/Program Files
+program = os.path.join("C:/Program Files/Détectruite")
+
 # Chemin vers le script Python
 script = f"{repertoire}/code detecte/Détectruite.py"
-
 
 icon = f"{repertoire}/code detecte/image/truite.ico"
 
@@ -25,7 +28,7 @@ options = {
         "packages": [],  # Liste des packages supplémentaires à inclure
         "excludes": [],  # Liste des modules à exclure
         "include_files": [],  # Liste des fichiers supplémentaires à inclure
-        "build_exe": os.path.join(repertoire, "build") #crée le fichier executable dans le répertoir build
+        "build_exe": program  # Crée le fichier exécutable dans le dossier "C:/Program Files/detectruite"
     }
 }
 
@@ -36,11 +39,13 @@ setup(
     options=options,
 )
 
-# Chemin vers le fichier exécutable généré
-executable_path = os.path.join(f"{repertoire}/build/Détectruite.exe")
+# Chemin vers le fichier exécutable
+executable_path = f"{program}\\Détectruite.exe"
 
-# Chemin vers le bureau
-desktop_path = Path.home() / "Desktop"
+# Nom du raccourci
+shortcut_name = "Détectruite"
 
-# Création du raccourci sur le bureau
-pyshortcuts.make_shortcut(executable_path, name="Détectruite", folder=str(desktop_path), icon=icon)
+# Emplacement où vous souhaitez sauvegarder le raccourci (par exemple, le bureau)
+shortcut_location = Path.home() / "Desktop"
+
+make_shortcut(executable_path, name='Détectruite', icon=icon, executable=executable_path)
