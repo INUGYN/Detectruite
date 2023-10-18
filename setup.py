89ec1d36bde28b60c2f821794c2fe2e4a3854614
INUGYN <116585597+INUGYN@users.noreply.github.com>
2023-10-18 21:26:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,37 @@
 import os
+import shutil
 import pyshortcuts
+from pathlib import Path
 from pyshortcuts import make_shortcut
 from cx_Freeze import setup, Executable
-from pathlib import Path
 
 repertoire = os.path.dirname(os.path.abspath(__file__))
 repertoire = os.path.normpath(repertoire)
+repertoire_files = f"{repertoire}/code detecte"
 
 # Chemin vers le dossier de destination dans C:/Program Files
-program = os.path.join("C:/Program Files/Détectruite")
+program = os.path.join("C:/Program Files")
+code = os.path.join(program, "Détectruite")
+code_destination = os.path.join(program, "Détectruite/code detecte")
+
+# Supprimer le répertoire de destination s'il existe déjà
+if os.path.exists(code):
+    shutil.rmtree(code)
+        
+
+# Créez le répertoire de destination s'il n'existe pas déjà
+os.makedirs(code, exist_ok=True)
+
+# Copiez le répertoire avec son contenu
+shutil.copytree(repertoire_files, code_destination)
+
+build = os.path.join(program, "Détectruite/build")
+os.makedirs(build, exist_ok=True)
 
 # Chemin vers le script Python
-script = f"{repertoire}/code detecte/Détectruite.py"
+script = f"{program}/Détectruite/code detecte/Détectruite.py"
 
-icon = f"{repertoire}/code detecte/image/truite.ico"
+icon = f"{program}/Détectruite/code detecte/image/truite.ico"
 
 # Configuration de l'exécutable
 exe = Executable(
@@ -28,7 +46,7 @@ options = {
         "packages": [],  # Liste des packages supplémentaires à inclure
         "excludes": [],  # Liste des modules à exclure
         "include_files": [],  # Liste des fichiers supplémentaires à inclure
-        "build_exe": program  # Crée le fichier exécutable dans le dossier "C:/Program Files/detectruite"
+        "build_exe": build  # Crée le fichier exécutable dans le dossier "C:/Program Files/detectruite"
     }
 }
 
@@ -40,7 +58,9 @@ setup(
 )
 
 # Chemin vers le fichier exécutable
-executable_path = f"{program}\\Détectruite.exe"
+executable_path = f"{build}/Détectruite.exe"
+
+
 
 # Nom du raccourci
 shortcut_name = "Détectruite"
@@ -48,4 +68,9 @@ shortcut_name = "Détectruite"
 # Emplacement où vous souhaitez sauvegarder le raccourci (par exemple, le bureau)
 shortcut_location = Path.home() / "Desktop"
 
+try:
+    racourcis = Path.home() / "Desktop/Détectruite.lnk"
+    os.remove(racourcis)
+except:
+    pass
 make_shortcut(executable_path, name='Détectruite', icon=icon, executable=executable_path)
